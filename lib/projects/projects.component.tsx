f79bdfde0c94479a7a117f5bb61686b08b6edd01
Ross MacPhee <ross@getinflow.io>
2023-10-29 22:54:18 +0000

    Display external link arrow

--- lib/projects/projects.component.tsx
+++ lib/projects/projects.component.tsx
@@ -1,6 +1,7 @@
 import {getAnimationOrder} from '../utils/getAnimationOrder'
 import projects from './snippets/projects.json'
 import styles from './styles/Projects.module.css'
+import {ArrowUpRight} from 'react-feather'
 
 const ANIMATION_ORDER = getAnimationOrder(3)
 
@@ -11,7 +12,7 @@ export default function Projects() {
       <ul className={styles.projects}>
         {projects.map((project, index) => (
           <li key={index}>
-            <h3><a href={project.href}>{project.name}</a></h3>
+            <h3><a href={project.href}>{project.name} <ArrowUpRight strokeWidth={3} size={'.9rem'} /></a></h3>
             <p>{project.description}</p>
           </li>
         ))}
